c6e728195ef6f657c5cf6ded344d6334d0dbab13
FaabriceC <fabricecannan@outlook.com>
2024-05-07 15:29:04 +0200

    Changement de la map + Transformation du tableau de Integer en une ArrayList

--- src/main/java/com/example/zeldasae/controller/Controller.java
+++ src/main/java/com/example/zeldasae/controller/Controller.java
@@ -1,13 +1,19 @@
 package com.example.zeldasae.controller;
 
+import com.example.zeldasae.Main;
 import com.example.zeldasae.modele.Monde;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.TilePane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
+import java.lang.reflect.Array;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
@@ -20,25 +26,31 @@ public class Controller implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         this.map = new Monde();
-
         afficherMap();
     }
 
 
+    /**
+     * Méthode qui permet d'afficher la map
+     */
+
     public void afficherMap() {
 
-        int[][] m = this.map.getMap();
+        ArrayList<Integer> m = this.map.getMap();
 
-        for (int x = 0 ; x < m.length ; x++) {
-            for (int y = 0 ; y < m[x].length ; y++) {
-                Rectangle rectangle = new Rectangle(30, 30);
-                switch (m[x][y]) {
-                    case 0 -> rectangle.setFill(Color.GREEN);
-                    case 1 -> rectangle.setFill(Color.GRAY);
+        for (int x = 0 ; x < m.size() ; x++) {
+                ImageView imageView = new ImageView();
+                switch (m.get(x)) {
+                    case 0:
+                        Image image = new Image("file:/home/etudiants/info/fcannan/IdeaProjects/ZeldaSAE2.1/src/main/resources/com/example/zeldasae/img/grass.jpg");
+                        imageView.setImage(image);
+                        break;
                 }
-                mapPane.getChildren().add(rectangle);
+
+                this.mapPane.getChildren().add(imageView);
+
             }
-        }
+
     }
 
 }
